c0ae81253393364f203eef9feeb9f467ce7e299d
Yoochanhong <chanhongy6@naver.com>
2022-10-22 22:48:55 +0900

    카드 위젯 틀 완성

--- lib/list_card.dart
+++ lib/list_card.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
 
 class ListCard extends StatelessWidget {
-  const ListCard({Key? key}) : super(key: key);
+  ListCard({required this.title});
+
+  var title;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: 80,
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.only(
+            bottom: 20,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 25, left: 10),
+            child: Text(
+              '$title',
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
