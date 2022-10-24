aa579785fff145a0a8784f9dd213374b9e77e5e4
Yoochanhong <chanhongy6@naver.com>
2022-10-24 20:03:30 +0900

    Custom_dialog 페이지 추가

---
+++ lib/custom_dialog.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class custom_dialog extends StatelessWidget {
+  const custom_dialog({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
