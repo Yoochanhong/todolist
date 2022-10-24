3426dfebb82ec861d5cf972d66f2760ea410d227
Yoochanhong <chanhongy6@naver.com>
2022-10-24 20:13:41 +0900

    제스처디텍터 onLongPress dialog로 수정

--- lib/list_card.dart
+++ lib/list_card.dart
@@ -15,7 +15,24 @@ class ListCard extends StatelessWidget {
       height: 80,
       child: GestureDetector(
         onLongPress: () {
-          firescore.collection(collection).doc(doc).delete();
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  content: SingleChildScrollView(
+                    child: Text(
+                      '삭제하시겠습니까?',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  actions: [
+                    ElevatedButton(onPressed: () {}, child: Text('예')),
+                    ElevatedButton(onPressed: () {}, child: Text('아니오')),
+                  ],
+                );
+              });
         },
         child: Card(
           child: Padding(
