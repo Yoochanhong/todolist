927038ae7c48ca3c4305a181b033fbfc142d3e25
Yoochanhong <chanhongy6@naver.com>
2022-10-25 17:34:35 +0900

    선택수정은 더 공부해서 해보자

--- lib/list_card.dart
+++ lib/list_card.dart
@@ -6,16 +6,15 @@ class ListCard extends StatelessWidget {
 
   var title;
   var collection;
-  var firestore = FirebaseFirestore.instance;
+  var firescore = FirebaseFirestore.instance;
   var doc;
-  TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 80,
       child: GestureDetector(
-        onHorizontalDragEnd: (DragEndDetails dragEndDetails) {
+        onHorizontalDragEnd: (DragEndDetails dragEndDetails){
           showDialog(
               context: context,
               builder: (BuildContext context) {
@@ -31,60 +30,8 @@ class ListCard extends StatelessWidget {
                   actions: [
                     ElevatedButton(
                         onPressed: () {
-                          firestore.collection(collection).doc(doc).delete();
+                          firescore.collection(collection).doc(doc).delete();
                           Navigator.of(context).pop();
-                        },
-                        child: Text('예')),
-                    ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: Text('아니오')),
-                  ],
-                );
-              });
-        },
-        onLongPress: () {
-          showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  content: SingleChildScrollView(
-                    child: Text(
-                      '수정하시겠습니까?',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                  actions: [
-                    ElevatedButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                          showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  content: SingleChildScrollView(
-                                    child: TextField(
-                                      controller: controller,
-                                      decoration: InputDecoration(
-                                        hintText: title,
-                                      ),
-                                      style: TextStyle(
-                                        fontSize: 20,
-                                      ),
-                                    ),
-                                  ),
-                                  actions: [
-                                    ElevatedButton(
-                                        onPressed: () {
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: Text('확인')),
-                                  ],
-                                );
-                              });
                         },
                         child: Text('예')),
                     ElevatedButton(
