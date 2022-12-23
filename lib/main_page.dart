57bbf6e0e9d00870c9e36e135ba93c18695b3916
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-23 18:01:12 +0900

    다시 시작

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -21,67 +21,65 @@ class _MainPageState extends State<MainPage> {
       appBar: AppBar(
         title: Text('투두리스트'),
       ),
-      body: Container(
-        child: Center(
-          child: Column(
-            children: [
-              StreamBuilder<QuerySnapshot>(
-                  stream: firestore.collection(collection).snapshots(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return CircularProgressIndicator();
-                    }
-                    return SizedBox(
-                      width: 300,
-                      height: 500,
-                      child: ListView.builder(
-                        itemCount: snapshot.data!.docs.length,
-                        itemBuilder: (context, index) {
-                          return ListCard(
-                            title:
-                                snapshot.data!.docs[index]['title'].toString(),
-                            collection: collection,
-                            doc: snapshot.data!.docs[index]['title'].toString(),
-                          );
-                        },
-                      ),
-                    );
-                  }),
-              Padding(
-                padding: const EdgeInsets.only(top: 30),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SizedBox(
-                      width: 250.0,
-                      child: TextField(
-                        controller: controller,
-                        keyboardType: TextInputType.text,
-                        decoration: InputDecoration(
-                          hintText: '텍스트를 입력해주세요',
-                        ),
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        firestore
-                            .collection(collection)
-                            .doc(controller.text)
-                            .set({'title': controller.text});
-                        controller.clear();
+      body: Center(
+        child: Column(
+          children: [
+            StreamBuilder<QuerySnapshot>(
+                stream: firestore.collection(collection).snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  }
+                  return SizedBox(
+                    width: 300,
+                    height: 500,
+                    child: ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        return ListCard(
+                          title:
+                              snapshot.data!.docs[index]['title'].toString(),
+                          collection: collection,
+                          doc: snapshot.data!.docs[index]['title'].toString(),
+                        );
                       },
-                      child: const Text(
-                        '입력',
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
+                    ),
+                  );
+                }),
+            Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: 250.0,
+                    child: TextField(
+                      controller: controller,
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        hintText: '텍스트를 입력해주세요',
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      firestore
+                          .collection(collection)
+                          .doc(controller.text)
+                          .set({'title': controller.text});
+                      controller.clear();
+                    },
+                    child: const Text(
+                      '입력',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
