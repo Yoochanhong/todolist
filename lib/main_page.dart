2f300a26dfbe75387958323545ccde35b566bf31
맥북 유찬홍 <chanhongy6@naver.com>
2022-12-23 18:24:12 +0900

    streambuilder 분기 처리

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -10,77 +10,23 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  var firestore = FirebaseFirestore.instance;
-  var collection = 'todo';
+  var product = FirebaseFirestore.instance.collection('items');
   TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    final DocumentSnapshot documentSnapshot;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('투두리스트'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            StreamBuilder<QuerySnapshot>(
-                stream: firestore.collection(collection).snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return CircularProgressIndicator();
-                  }
-                  return SizedBox(
-                    width: 300,
-                    height: 500,
-                    child: ListView.builder(
-                      itemCount: snapshot.data!.docs.length,
-                      itemBuilder: (context, index) {
-                        return ListCard(
-                          title:
-                              snapshot.data!.docs[index]['title'].toString(),
-                          collection: collection,
-                          doc: snapshot.data!.docs[index]['title'].toString(),
-                        );
-                      },
-                    ),
-                  );
-                }),
-            Padding(
-              padding: const EdgeInsets.only(top: 30),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SizedBox(
-                    width: 250.0,
-                    child: TextField(
-                      controller: controller,
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(
-                        hintText: '텍스트를 입력해주세요',
-                      ),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      firestore
-                          .collection(collection)
-                          .doc(controller.text)
-                          .set({'title': controller.text});
-                      controller.clear();
-                    },
-                    child: const Text(
-                      '입력',
-                      style: TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+      body: StreamBuilder(
+        stream: product.snapshots(),
+        builder: (BuildContext context,
+            AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.hasData){
+            return ListView.builder(itemBuilder: (BuildContext context, int index){
+              return Text('what');
+            });
+          }
+          return Center(child: CircularProgressIndicator());
+        },
       ),
     );
   }
